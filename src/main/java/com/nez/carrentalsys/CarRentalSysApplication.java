d162a94b38fb6484da416e2b796b1823d5ea90a1
Nour elyakine Zerguine <nourelyakine.zerguine@univ-constantine2.dz>
2025-08-04 13:02:09 +0100

    CREATION: mini-project has been created and structured.

---
+++ src/main/java/com/nez/carrentalsys/CarRentalSysApplication.java
@@ -0,0 +1,13 @@
+package com.nez.carrentalsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalSysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarRentalSysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nez/carrentalsys/CarRentalSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nez.carrentalsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalSysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
