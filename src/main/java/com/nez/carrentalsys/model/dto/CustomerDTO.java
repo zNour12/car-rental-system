7f96d475b5a21e9398e8ddd6bf5cf6cb6ac0e96e
zNour12 <nourelyakine.zerguine@univ-constantine2.dz>
2025-08-11 18:45:09 +0100

    ADDED: Created the CustomerDTO.java and coded it with necessities

---
+++ src/main/java/com/nez/carrentalsys/model/dto/CustomerDTO.java
@@ -0,0 +1,44 @@
+package com.nez.carrentalsys.model.dto;
+
+import com.nez.carrentalsys.model.enums.CustomerStatus;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomerDTO {
+    private Long id;
+
+    @NotBlank(message = "First name is required")
+    @Size(min = 2, max = 50, message = "First name must be between 2 and 50 characters")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    @Size(min = 2, max = 50, message = "Last name must be between 2 and 50 characters")
+    private String lastName;
+
+    @Email(message = "Must be a valid email format")
+    @NotBlank
+    private String email;
+
+    @Pattern(regexp = "^\\+?[0-9]{8,15}$")
+    private String phoneNumber;
+
+    @NotBlank(message = "Driver license is required")
+    @Size(min = 5, max = 20, message = "Driver license must be between 5 and 20 characters")
+    private String driverLicense;
+
+    @Past(message = "Date of birth must be in the past")
+    @NotNull(message = "Date of birth is required")
+    private LocalDate birthDate;
+
+    @NotNull(message = "Status is required")
+    private CustomerStatus status;
+}
