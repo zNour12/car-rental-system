ff9208ad2d79e4e1816441f15bffa1dcd2e99af9
zNour12 <nourelyakine.zerguine@univ-constantine2.dz>
2025-08-11 18:27:04 +0100

    ADDED: Created the new Entity Customer and coded its essentials

---
+++ src/main/java/com/nez/carrentalsys/model/entity/Customer.java
@@ -0,0 +1,44 @@
+package com.nez.carrentalsys.model.entity;
+
+import com.nez.carrentalsys.model.enums.CustomerStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "customers")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "customer_first_name")
+    private String firstName;
+
+    @Column(name = "customer_last_name")
+    private String lastName;
+
+    @Column(name = "customer_email")
+    private String email;
+
+    @Column(name = "customer_phone_number")
+    private String phoneNumber;
+
+    @Column(name = "customer_driver_license_number")
+    private String driverLicenseNumber;
+
+    @Column(name = "customer_birth_date")
+    private LocalDate birthDate;
+
+    @Column(name = "customer_status")
+    @Enumerated(EnumType.STRING)
+    private CustomerStatus status;
+}
