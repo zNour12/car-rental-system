bc116c4d8e4fbe4d3bb0283179438fecd0cddd3f
zNour12 <nourelyakine.zerguine@univ-constantine2.dz>
2025-08-12 14:06:53 +0100

    ADDED: Created the CustomerMapper.java

---
+++ src/main/java/com/nez/carrentalsys/mapper/CustomerMapper.java
@@ -0,0 +1,11 @@
+package com.nez.carrentalsys.mapper;
+
+import com.nez.carrentalsys.model.dto.CustomerDTO;
+import com.nez.carrentalsys.model.entity.Customer;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface CustomerMapper {
+    CustomerDTO toDto(Customer customer);
+    Customer toEntity(CustomerDTO customerDTO);
+}
